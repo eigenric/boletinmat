d10f7f8b1cd9f823f50d02e4887d26cb8a129ae1
Ricardo Ruiz <riki319@gmail.com>
2016-07-05 19:07:51 +0200

    Exceptions managed

--- concurso.py
+++ concurso.py
@@ -1,10 +1,17 @@
-#encoding:utf-8
+# encoding:utf-8
 # Programa en Python que halla la solucion del problema
 # Problema: subconjuntos de N, con elementos consecutivos que sumen 91
 
 import sys
 
+
+isnatural = lambda num: isinstance(num, int) and num > 0
+
 def solveProblema(n):
+	'''N must be a natural number'''
+
+	if not isnatural(n): raise ValueError("El numero tiene que ser natural")
+
 	posibles = []
 	conjuntos = []
 
@@ -20,13 +27,27 @@ def formatConjunto(conjunto):
 	return '{'+str(conjunto[0]) + ', ... ' + str(conjunto[-1]) + '}'
 
 
+def consultarNumero():
+
+	try: 
+		numero = int(raw_input("Número a ingresar: "))
+	except ValueError:
+		print("Debe ser un numero natural")
+		exit()
+	except KeyboardInterrupt:
+		print("\nPrograma terminado")
+		exit()
+
+	return numero
+
+
 if __name__ == '__main__':
 
 	try:
 		numero = int(sys.argv[1])
 	except:
-		print "Error: python concurso.py <int>"
-		exit()
+		numero = consultarNumero()
+
 	solucion = solveProblema(numero)
 
 	print "Los siguientes subconjuntos en N de numeros consecutivos suman " + str(numero)
